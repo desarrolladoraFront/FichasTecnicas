ef03a60e5095407da5539dbfb1bd8ca427389103
desarrolladoraFront <danielaandrade66@gmail.com>
2023-04-11 13:23:45 -0600

    Termina el componente de Title and Table con las tres fichas y sus debidos parámetros.

--- Components/TitleAndTable.js
+++ Components/TitleAndTable.js
@@ -1,150 +1,167 @@
-export const TitleAndTable = (id, color) => {
-    const section = document.getElementById('section');  //crea una sección que será el nodo padre de los otros elementos que voy a crear.
-    section.style.backgroundColor = color;
+export const TitleAndTable = (clas, colorBackground, nameOfproduct, keySap, brand, category, flavor, model, subCategory, color) => {
+    //Take the section element to render the following elements in it.
+    const section = document.getElementById('section');
+    //Add the color that differentiates the data sheets.
+    section.style.backgroundColor = colorBackground;
 
-    const nombreProducto = document.createElement('h1');
-    nombreProducto.textContent = 'Chocolate abuelita'
-    nombreProducto.classList = 'title';
-    //crea la tabla de presentación, con las características del producto.
+    //Creates the h1 object that refers to the product name.
+    const h1 = document.createElement('h1');
+    h1.textContent = nameOfproduct;
+    h1.classList = 'title';
+
+    //Creates the presentation table, with the characteristics of the product.
     const tableContent = document.createElement('table');
     tableContent.classList = '.list-group';
-    //tabla para insumos
-        if (id === 1) {
-        //tabla para insumos
-            const celdaClave = document.createElement('tr');
-            const textoClave = document.createElement('td');
-            const clave = document.createElement('td');
-
-            const celdaMarca = document.createElement('tr');
-            const textoMarca = document.createElement('td');
-            const marca = document.createElement('td');
-
-            const celdaCategoria = document.createElement('tr');
-            const textoCategoria = document.createElement('td');
-            const categoria = document.createElement('td');
-
-            const celdaSabor = document.createElement('tr');
-            const textoSabor = document.createElement('td');
-            const sabor = document.createElement('td');
-
-        //contenido que va llevar la tabla
-            textoClave.textContent = "Clave:"; //aquí tendré que poner las variables que guarden los elementos de mis objetos.
-            clave.textContent = "Hfsjdfals";
-        
-            textoMarca.textContent = 'Marca:';
-            marca.textContent = 'VANS'
-
-            textoCategoria.textContent = 'Categoría:';
-            categoria.textContent = 'Leches'
-
-            textoSabor.textContent = 'Sabor';
-            sabor.textContent = 'Chocolate'
-
-            celdaClave.append(textoClave, clave)
-            celdaMarca.append(textoMarca, marca)
-            celdaCategoria.append(textoCategoria, categoria)
-            celdaSabor.append(textoSabor, sabor)
-
-            tableContent.append(celdaClave, celdaMarca, celdaCategoria, celdaSabor);
-           return section.append(nombreProducto, nombreProducto, tableContent);
-
-        } if (id === 2) {
-             //tabla para maquinaria
-        const celdaClave = document.createElement('tr');
-        const textoClave = document.createElement('td');
-        const clave = document.createElement('td');
-
-        const celdaMarca = document.createElement('tr');
-        const textoMarca = document.createElement('td');
-        const marca = document.createElement('td');
-
-        const celdaModelo = document.createElement('tr');
-        const textoModelo = document.createElement('td');
-        const modelo = document.createElement('td');
-        
-        const celdaCategoria = document.createElement('tr');
-        const textoCategoria = document.createElement('td');
-        const categoria = document.createElement('td');
-
-        const celdaSubcategoria = document.createElement('tr');
-        const textoSubcategoria = document.createElement('td');
-        const subcategoria = document.createElement('td');
-
-        const celdaColor = document.createElement('tr');
-        const textoColor = document.createElement('td');
-        const color = document.createElement('td');
-
-    //contenido que va llevar la tabla
-        textoClave.textContent = "Clave:"; //aquí tendré que poner las variables que guarden los elementos de mis objetos.
-        clave.textContent = "Hfsjdfals";
     
-        textoMarca.textContent = 'Marca:';
-        marca.textContent = 'VANS'
+        //Table for insumos.
+        if (clas === 1) {
+            //Variables for KeySap.
+            const CONTAINER_KEY = document.createElement('tr');
+            const TEXT_KEY = document.createElement('td');
+            const KEY = document.createElement('td');
 
-        textoModelo.textContent = 'Modelo:';
-        modelo.textContent = 'Novo';
+            //Variables for Brand
+            const CONTAINER_BRAND = document.createElement('tr');
+            const TEXT_BRAND = document.createElement('td');
+            const BRAND = document.createElement('td');
 
-        textoCategoria.textContent = 'Categoría:';
-        categoria.textContent = 'Leches'
+            //Variables for category
+            const CONTEINER_CATEGORY = document.createElement('tr');
+            const TEXT_CATEGORY = document.createElement('td');
+            const CATEGORY = document.createElement('td');
 
-        textoSubcategoria.textContent = 'Subcategoría:';
-        subcategoria.textContent = 'Molinos';
+            const CONTAINER_FLAVOR = document.createElement('tr');
+            const TEXT_FLAVOR = document.createElement('td');
+            const FLAVOR = document.createElement('td');
 
-        textoColor.textContent = 'Color:';
-        color.textContent = 'Amarillo';
-
-
-        celdaClave.append(textoClave, clave);
-        celdaMarca.append(textoMarca, marca);
-        celdaModelo.append(textoModelo, modelo);
-        celdaCategoria.append(textoCategoria, categoria);
-        celdaSubcategoria.append(textoSubcategoria, subcategoria);
-        celdaColor.append(textoColor, color)
-
+            //Content to be included in the table:
+            TEXT_KEY.textContent = "Clave: ";
+            KEY.textContent = keySap;
         
+            TEXT_BRAND.textContent = 'Marca :';
+            BRAND.textContent = brand;
 
-        tableContent.append(celdaClave, celdaMarca, celdaModelo, celdaCategoria, celdaSubcategoria, celdaColor);
-        return section.append(nombreProducto, nombreProducto, tableContent);
+            TEXT_CATEGORY.textContent = 'Categoría :';
+            CATEGORY.textContent = category;
 
-        } if (id === 3 ) {
-        //tabla para maquinaria
-        const celdaClave = document.createElement('tr');
-        const textoClave = document.createElement('td');
-        const clave = document.createElement('td');
+            TEXT_FLAVOR.textContent = 'Sabor';
+            FLAVOR.textContent = flavor;
 
-        const celdaMarca = document.createElement('tr');
-        const textoMarca = document.createElement('td');
-        const marca = document.createElement('td');
+            //Renders the elements of the table in your cell.
+            CONTAINER_KEY.append(TEXT_KEY, KEY);
+            CONTAINER_BRAND.append(TEXT_BRAND, BRAND);
+            CONTEINER_CATEGORY.append(TEXT_CATEGORY, CATEGORY);
+            CONTAINER_FLAVOR.append(TEXT_FLAVOR, FLAVOR);
 
-        const celdaCategoria = document.createElement('tr');
-        const textoCategoria = document.createElement('td');
-        const categoria = document.createElement('td');
+            //Renders the table.
+            tableContent.append(CONTAINER_KEY, CONTAINER_BRAND, CONTEINER_CATEGORY, CONTAINER_FLAVOR);
 
-        const celdaSubcategoria = document.createElement('tr');
-        const textoSubcategoria = document.createElement('td');
-        const subcategoria = document.createElement('td');
+            // Returns the section to appear in the DOM.
+            return section.append(h1, tableContent);
 
-        //contenido que va llevar la tabla
-        textoClave.textContent = "Clave:"; //aquí tendré que poner las variables que guarden los elementos de mis objetos.
-        clave.textContent = "Hfsjdfals";
+            //Table for machinery
+            } if (clas === 2) {
+                //Variables for KeySap.
+                const CONTAINER_KEY = document.createElement('tr');
+                const TEXT_KEY = document.createElement('td');
+                const KEY = document.createElement('td');
+
+                //Variables for Brand
+                const CONTAINER_BRAND = document.createElement('tr');
+                const TEXT_BRAND = document.createElement('td');
+                const BRAND = document.createElement('td');
+
+                //Variables for Model.
+                const CONTAINER_MODEL = document.createElement('tr');
+                const TEXT_MODEL = document.createElement('td');
+                const MODEL = document.createElement('td');
+
+                //Variables for Category.
+                const CONTAINER_CATEGORY = document.createElement('tr');
+                const TEXT_CATEGORY = document.createElement('td');
+                const CATEGORY = document.createElement('td');
+
+                //Variables for Color.
+                const CONTAINER_COLOR = document.createElement('tr');
+                const TEXT_COLOR = document.createElement('td');
+                const COLOR = document.createElement('td');
+
+                //Content to be included in the table:
+                TEXT_KEY.textContent = "Clave :";
+                KEY.textContent = keySap;
     
-        textoMarca.textContent = 'Marca:';
-        marca.textContent = 'VANS'
+                TEXT_BRAND.textContent = 'Marca :';
+                BRAND.textContent = brand;
 
-        textoCategoria.textContent = 'Categoría:';
-        categoria.textContent = 'Leches'
+                TEXT_MODEL.textContent = 'Modelo :';
+                MODEL.textContent = model;
 
-        textoSubcategoria.textContent = 'Subcategoría:';
-        subcategoria.textContent = 'Molinos';
+                TEXT_COLOR.textContent = 'Categoría :';
+                CATEGORY.textContent = category;
 
-        celdaClave.append(textoClave, clave);
-        celdaMarca.append(textoMarca, marca);
-        celdaCategoria.append(textoCategoria, categoria);
-        celdaSubcategoria.append(textoSubcategoria, subcategoria);
-        
-        tableContent.append(celdaClave, celdaMarca, celdaCategoria, celdaSubcategoria);
-        return section.append(nombreProducto, nombreProducto, tableContent);
+                TEXT_COLOR.textContent = 'Color :';
+                COLOR.textContent = color;
+
+                 //Renders the elements of the table in your cell.
+                CONTAINER_KEY.append(TEXT_KEY, KEY);
+                CONTAINER_BRAND.append(TEXT_BRAND, BRAND);
+                CONTAINER_MODEL.append(TEXT_MODEL, MODEL);
+                CONTAINER_CATEGORY.append(TEXT_CATEGORY, CATEGORY);
+                CONTAINER_COLOR.append(TEXT_COLOR, COLOR);
+
+                 //Renders the table.
+                tableContent.append(CONTAINER_KEY, CONTAINER_BRAND, CONTAINER_MODEL, CONTAINER_CATEGORY, CONTAINER_COLOR);
+
+                //Returns the section to appear in the DOM.
+                return section.append(h1, tableContent);
+
+                //Table for Accesories.
+                } if (clas === 3 ) {
+                    //Variables for keySap.
+                    const CONTAINER_KEY = document.createElement('tr');
+                    const TEXT_KEY = document.createElement('td');
+                    const KEY = document.createElement('td');
+
+                    //Variables for Brand.
+                    const CONTAINER_BRAND = document.createElement('tr');
+                    const TEXT_BRAND = document.createElement('td');
+                    const BRAND = document.createElement('td');
+
+                    //Variables for Category.
+                    const CONTAINER_CATEGORY = document.createElement('tr');
+                    const TEXT_CATEGORY = document.createElement('td');
+                    const CATEGORY = document.createElement('td');
+
+                    //Variables for Subcategory.
+                    const CONTAINER_SUBCATEGORY = document.createElement('tr');
+                    const TEXT_SUBCATEGORY = document.createElement('td');
+                    const SUBCATEGORY = document.createElement('td');
+
+
+                     //Content to be included in the table:
+                    TEXT_CATEGORY.textContent = "Clave :"; 
+                    KEY.textContent = keySap;
+    
+                    TEXT_BRAND.textContent = 'Marca :';
+                    BRAND.textContent = brand;
+
+                    TEXT_CATEGORY.textContent = 'Categoría:';
+                    CATEGORY.textContent = category;
+
+                    TEXT_SUBCATEGORY.textContent = 'Subcategoría:';
+                    SUBCATEGORY.textContent = subCategory;
+
+                    //Renders the elements of the table in your cell.
+                    CONTAINER_CATEGORY.append(TEXT_CATEGORY, KEY);
+                    CONTAINER_BRAND.append(TEXT_BRAND, BRAND);
+                    CONTAINER_CATEGORY.append(TEXT_CATEGORY, CATEGORY);
+                    CONTAINER_SUBCATEGORY.append(TEXT_SUBCATEGORY, SUBCATEGORY);
+                    
+                    //Renders the table.
+                    tableContent.append(CONTAINER_CATEGORY, CONTAINER_BRAND, CONTAINER_CATEGORY, CONTAINER_SUBCATEGORY);
+
+                    //Returns the section to appear in the DOM.
+                    return section.append(h1, tableContent);
         }
 }
 
-//ahora me falta el estilo y cuidar los parámetros que entraran como variables.
